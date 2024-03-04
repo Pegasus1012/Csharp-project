5ed47d428c7758a363ed6b08c60fa69950e4da76
Pegasus1012 <161907410+Pegasus1012@users.noreply.github.com>
2024-03-04 20:37:31 +0800

    Add files via upload

---
+++ assignment1/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.btnjia = new System.Windows.Forms.Button();
+            this.btnjian = new System.Windows.Forms.Button();
+            this.btnchen = new System.Windows.Forms.Button();
+            this.btnchu = new System.Windows.Forms.Button();
+            this.btncalcu = new System.Windows.Forms.Button();
+            this.tetResult = new System.Windows.Forms.TextBox();
+            this.buttonclean = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(362, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(475, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(255, 182);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(362, 182);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(52, 50);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(475, 182);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(52, 50);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(255, 253);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(52, 50);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(362, 253);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(52, 50);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(475, 253);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(52, 50);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(255, 331);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(52, 50);
+            this.button0.TabIndex = 9;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // btnjia
+            // 
+            this.btnjia.Location = new System.Drawing.Point(577, 114);
+            this.btnjia.Name = "btnjia";
+            this.btnjia.Size = new System.Drawing.Size(52, 50);
+            this.btnjia.TabIndex = 10;
+            this.btnjia.Text = "+";
+            this.btnjia.UseVisualStyleBackColor = true;
+            this.btnjia.Click += new System.EventHandler(this.btnjia_Click);
+            // 
+            // btnjian
+            // 
+            this.btnjian.Location = new System.Drawing.Point(577, 182);
+            this.btnjian.Name = "btnjian";
+            this.btnjian.Size = new System.Drawing.Size(52, 50);
+            this.btnjian.TabIndex = 11;
+            this.btnjian.Text = "-";
+            this.btnjian.UseVisualStyleBackColor = true;
+            this.btnjian.Click += new System.EventHandler(this.btnjian_Click);
+            // 
+            // btnchen
+            // 
+            this.btnchen.Location = new System.Drawing.Point(577, 253);
+            this.btnchen.Name = "btnchen";
+            this.btnchen.Size = new System.Drawing.Size(52, 50);
+            this.btnchen.TabIndex = 12;
+            this.btnchen.Text = "*";
+            this.btnchen.UseVisualStyleBackColor = true;
+            this.btnchen.Click += new System.EventHandler(this.btnchen_Click);
+            // 
+            // btnchu
+            // 
+            this.btnchu.Location = new System.Drawing.Point(577, 331);
+            this.btnchu.Name = "btnchu";
+            this.btnchu.Size = new System.Drawing.Size(52, 50);
+            this.btnchu.TabIndex = 13;
+            this.btnchu.Text = "/";
+            this.btnchu.UseVisualStyleBackColor = true;
+            this.btnchu.Click += new System.EventHandler(this.btnchu_Click);
+            // 
+            // btncalcu
+            // 
+            this.btncalcu.Location = new System.Drawing.Point(362, 331);
+            this.btncalcu.Name = "btncalcu";
+            this.btncalcu.Size = new System.Drawing.Size(165, 50);
+            this.btncalcu.TabIndex = 14;
+            this.btncalcu.Text = "计算";
+            this.btncalcu.UseVisualStyleBackColor = true;
+            this.btncalcu.Click += new System.EventHandler(this.btncalcu_Click);
+            // 
+            // tetResult
+            // 
+            this.tetResult.Location = new System.Drawing.Point(362, 73);
+            this.tetResult.Name = "tetResult";
+            this.tetResult.Size = new System.Drawing.Size(165, 28);
+            this.tetResult.TabIndex = 15;
+            this.tetResult.TextChanged += new System.EventHandler(this.tetResult_TextChanged);
+            // 
+            // buttonclean
+            // 
+            this.buttonclean.Location = new System.Drawing.Point(554, 73);
+            this.buttonclean.Name = "buttonclean";
+            this.buttonclean.Size = new System.Drawing.Size(88, 35);
+            this.buttonclean.TabIndex = 16;
+            this.buttonclean.Text = "清空";
+            this.buttonclean.UseVisualStyleBackColor = true;
+            this.buttonclean.Click += new System.EventHandler(this.buttonclean_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(250, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 28);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "结果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonclean);
+            this.Controls.Add(this.tetResult);
+            this.Controls.Add(this.btncalcu);
+            this.Controls.Add(this.btnchu);
+            this.Controls.Add(this.btnchen);
+            this.Controls.Add(this.btnjian);
+            this.Controls.Add(this.btnjia);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button btnjia;
+        private System.Windows.Forms.Button btnjian;
+        private System.Windows.Forms.Button btnchen;
+        private System.Windows.Forms.Button btnchu;
+        private System.Windows.Forms.Button btncalcu;
+        private System.Windows.Forms.TextBox tetResult;
+        private System.Windows.Forms.Button buttonclean;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ assignment1/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text += btn.Text;
+        }
+
+        private void tetResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text += btn.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text += btn.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text += btn.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text += btn.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text += btn.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text += btn.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text += btn.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text += btn.Text;
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text += btn.Text;
+        }
+
+        private void btncalcu_Click(object sender, EventArgs e)
+        {
+            Single r;
+
+            // 获取文本框 txtResult 中的文本
+            string t = tetResult.Text;
+
+            // 寻找文本中第一个空格的位置
+            int space = t.IndexOf(' ');
+
+            // 从文本中提取第一个操作数 s1（空格前的部分）
+            string s1 = t.Substring(0, space);
+
+            // 从文本中提取操作符 op（空格后的第一个字符）
+            char op = Convert.ToChar(t.Substring(space + 1, 1));
+
+            // 从文本中提取第二个操作数 s2（空格后两个字符以后的部分）
+            string s2 = t.Substring(space + 3);
+
+            // 将操作数转换为 Single 类型
+            Single arg1 = Convert.ToSingle(s1);
+            Single arg2 = Convert.ToSingle(s2);
+
+            // 使用 switch 语句根据操作符进行计算
+            switch (op)
+            {
+                case '+':
+                    r = arg1 + arg2;
+                    break;
+                case '-':
+                    r = arg1 - arg2;
+                    break;
+                case '*':
+                    r = arg1 * arg2;
+                    break;
+                case '/':
+                    // 如果除数为零，则抛出异常
+                    if (arg2 == 0)
+                    {
+                        throw new ApplicationException();
+                    }
+                    else
+                    {
+                        // 否则执行除法操作
+                        r = arg1 / arg2;
+                        break;
+                    }
+                default:
+                    // 如果操作符不是+、-、*、/之一，抛出异常
+                    throw new ApplicationException();
+            }
+
+            // 将计算结果显示在文本框 txtResult 中
+            tetResult.Text = r.ToString();
+        }
+
+        private void btnjia_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text = tetResult.Text + " " + btn.Text + " ";
+        }
+
+        private void btnjian_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text = tetResult.Text + " " + btn.Text + " ";
+        }
+
+        private void btnchen_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text = tetResult.Text + " " + btn.Text + " ";
+        }
+
+        private void btnchu_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            tetResult.Text = tetResult.Text + " " + btn.Text + " ";
+        }
+
+        private void buttonclean_Click(object sender, EventArgs e)
+        {
+            tetResult.Text = "";
+        }
+    }
+}
